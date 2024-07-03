cd349c20f58e10680b3941326ec54bf6321435b1
moka0690 <madradz39@gmail.com>
2024-07-03 20:59:23 +0100

    Exercise solution  1

---
+++ ex1.js
@@ -0,0 +1,33 @@
+let mydiv = document.getElementById("elzero");
+let maydiv1 = document.getElementsByClassName("element")[0];
+let maydiv3 = document.getElementsByTagName("div")[0];
+let maydiv4 = document.querySelector("#elzero");
+let maydiv5 = document.querySelector(".element");
+let maydiv6 = document.querySelector("div");
+let maydiv7 = document.querySelectorAll("div")[0];
+let maydiv8 = document.querySelectorAll("#elzero")[0];
+let maydiv9 = document.querySelectorAll(".element")[0];
+let maydiv10 = document.querySelector("body").children[0];
+let maydiv11 = document.querySelector("body").firstElementChild;
+let maydiv12 = document.querySelector("body").childNodes[1];
+let maydiv13 = document.querySelector("body").lastElementChild;
+
+
+
+
+
+console.log(mydiv);
+console.log(maydiv1);
+console.log(maydiv3);
+console.log(maydiv4);
+console.log(maydiv5);
+console.log(maydiv6);
+console.log(maydiv7);
+console.log(maydiv8);
+console.log(maydiv9);
+console.log(maydiv10);
+console.log(maydiv11);
+console.log(maydiv12);
+console.log(maydiv13);
+
+
