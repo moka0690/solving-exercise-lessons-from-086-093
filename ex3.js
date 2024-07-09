22d53407a133f89d1672fc439b639afdd7a25f6e
moka0690 <madradz39@gmail.com>
2024-07-09 21:34:15 +0100

    Exercise solution  2

---
+++ ex3.js
@@ -0,0 +1,12 @@
+const inputfield = document.querySelector("input");
+const Viewconversion = document.querySelector(".result")
+function Currencyconversion(){
+ const CurrencyinDollars = inputfield.value;
+ const CurrencyinEgp = CurrencyinDollars * 15.6;
+ const result =`${CurrencyinDollars} USD Dollar = ${CurrencyinEgp.toFixed(2)} Egyptian Pound`
+ Viewconversion.innerText = result;
+
+}
+inputfield.oninput = function(){
+  Currencyconversion();
+ }
