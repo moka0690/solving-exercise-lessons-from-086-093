727f8c92bb001ab9bcabc7c2ca2467f846c43b9f
moka0690 <madradz39@gmail.com>
2024-07-17 20:31:43 +0100

    Modify the code to become numeric

--- ex5.js
+++ ex5.js
@@ -1,6 +1,6 @@
 const images = document.querySelectorAll('img');
 images.forEach(function(img) {
-    if (img.getAttribute('alt') === 'One' || img.getAttribute('alt') === 'Three') {
+    if (img.getAttribute('alt') ) {
       img.setAttribute('alt', 'Old');
     }else {
       img.setAttribute('alt', 'Elzero New');
